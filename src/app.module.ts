77c984a1a0edc68e8bfa23a71fb2e7ba39126916
G-KIBICHII <kibichiigilbert54@gmail.com>
2026-01-21 12:50:41 +0300

    save user to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,9 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost:27017/mail-server')],
+  imports: [UsersModule, MongooseModule.forRoot(
+      'mongodb://admin:admin123@localhost:27017/mail-server?authSource=admin'
+    ),],
   controllers: [AppController],
   providers: [AppService],
 })
