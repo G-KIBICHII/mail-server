1a16495ecbc48bee7880e6d773aa1ae37ab69152
G-KIBICHII <kibichiigilbert54@gmail.com>
2026-01-21 13:19:57 +0300

    auth module service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [UsersModule, MongooseModule.forRoot(
       'mongodb://admin:admin123@localhost:27017/mail-server?authSource=admin'
-    ),],
+    ), AuthModule,],
   controllers: [AppController],
   providers: [AppService],
 })
