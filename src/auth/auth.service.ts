e950bbddf3bad2a82ba9a3a96b4987861b19c3b1
G-KIBICHII <kibichiigilbert54@gmail.com>
2026-01-21 13:39:49 +0300

    set up jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { SignInDto } from './dto/sign-in.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+    constructor(private usersService: UsersService, private jwtService: JwtService) {}
 
     // Sign in
     async signIn(SignInDto:SignInDto): Promise<any> {
@@ -14,8 +15,11 @@ export class AuthService {
             throw new UnauthorizedException()
         }
 
-        const { password, ...result } = user;
-        return result;
+        const payload = { username: user.username, sub: user.id };
+        return {access_token: this.jwtService.sign(payload)};
+
+        // const { password, ...result } = user;
+        // return result;
     }
 
 }
